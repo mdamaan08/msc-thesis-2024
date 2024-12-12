2ee9e713c242ac59055f4594c6ac1b3a096dc408
root <root@ip-172-31-20-58.eu-west-1.compute.internal>
2024-12-12 05:31:36 +0000

    Updated Repo

--- examples/python-web-app/devops/demo/views.py
+++ examples/python-web-app/devops/demo/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+@require_http_methods(["GET", "POST"])
 def index(request):
     return render(request, 'demo_site.html')
